833f9bdc0cf836b20d2e4e438186a8eff8142cb1
otaviobia <otaviobiagioni31@gmail.com>
2026-01-07 02:28:10 +0000

    update from server

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlmodel import SQLModel, create_engine, Session
 
-sqlite_file_name = "database.db"
+sqlite_file_name = "/app/data/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
+
+os.makedirs(os.path.dirname(sqlite_file_name), exist_ok=True)
 
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
